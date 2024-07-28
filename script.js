ae863eb27174b0dd008be994f6c052a4de9ff35f
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-07-27 22:33:18 -0400

    added part 2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
 // Part 1 - Getting Started
 
 const mainEl = document.querySelector("main");
@@ -14,3 +22,28 @@ topMenuEl.style.height = "100%";
 
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
+
+// Part 3 - Adding Menu Buttons
+
+menuLinks.forEach((link) => {
+  const a = document.createElement("a");
+  a.setAttribute("href", link.href);
+  a.textContent = link.text;
+  topMenuEl.appendChild(a);
+});
+
+// END OF LAB (PART-1)
+
+/* LAB (PART-2) starts HERE*/
+
+/* part-1 (making sure we have the part 1 */
+
+/* part-2 adding a few elements in html and css */
+
+/* part-3 creating submenu */
+
+const subMenuEl = document.findElementById("sub-menu");
+subMenuEl.style.height = "100%";
+
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
